d87946f044550fd01f2d38a0b646d61750f6f26e
broglah1 <g@vinpickl.es>
2023-06-30 23:15:59 +0100

    remove files and rename function

--- BoxyPrompt/README.md
+++
@@ -1,7 +0,0 @@
-![](https://github.com/L-McG/PowerShell/blob/master/BoxyPrompt/Animation.gif)
-
-# Usage
-```
-Import-Module .\BoxyPrompt.ps1
-Boxy-Prompt
-```
--- SillyScripts/MockThem/MockThemButBadCode.ps1
+++
@@ -1,5 +0,0 @@
-function Mock-Them($str) {
-    $out = $null
-    $str.ToCharArray() | %{$out += if((Get-Random) % 2){$_.ToString().ToUpper()}else{$_.ToString().ToLower()}}
-    return $out
-}
